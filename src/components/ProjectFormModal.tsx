aff7f73536a5b76de16fed225f2cc414d360885f
TheNeonWolf <theneonwolf0404@gmail.com>
2026-09-23 22:04:58 +0800

    Fid minor bugs during deployment

--- src/components/ProjectFormModal.tsx
+++ src/components/ProjectFormModal.tsx
@@ -169,7 +169,7 @@ export default function ProjectFormModal({
 
           setExistingTasks([]);
           setOriginalTaskStatuses({});
-        } fontally {
+        } finally {
           setTasksLoading(false);
         }
       };
